7bbaffdb610f7b23175697c8e509156a651da16b
KirillGapich <kirillgapich@gmail.com>
2021-11-10 15:36:51 +0200

    registration and login complete v1

--- simple_network/account/views.py
+++ simple_network/account/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from .utils import DataMixin
+from .forms import RegisterUserForm, LoginUserForm
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_context = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(user_context.items()))
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = "login.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_context = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(user_context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('profile')
